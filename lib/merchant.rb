e249ea6983d071c1f5a8a153f67c967680ba1ea3
JerrelMitchell <jerrelmitchell@ymail.com>
2018-04-10 20:54:24 -0600

    update for spec harness

--- lib/merchant.rb
+++ lib/merchant.rb
@@ -1,8 +1,10 @@
 # :nodoc:
 class Merchant
+  attr_reader :parent
   attr_accessor :id, :name
-  def initialize(merchant)
-    @id   = merchant[:id].to_i
-    @name = merchant[:name]
+  def initialize(merchant, parent)
+    @id     = merchant[:id].to_i
+    @name   = merchant[:name]
+    @parent = parent
   end
 end
