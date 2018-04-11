b3c7bddbcfcf7b267fae72ca22e0688704e1f6c7
JamisonOrdway <jamisonordway@gmail.com>
2018-04-11 13:32:06 -0600

    Sales Analyst WIP

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -3,7 +3,21 @@ require_relative 'sales_engine'
 class SalesAnalyst
 
   def initialize
-    
+
   end
+
+  def average_items_per_merchant
+    @merchants.reduce(0) do |merchant|
+      merchant.items.length #can we use reduce here??
+    end
+    # find average items per merchant
+  end
+
+  def merchants_with_high_item_count
+    @merchants.sort_by do |merchant|
+      merchant.items.length
+    end
+  end
+
 
 end
