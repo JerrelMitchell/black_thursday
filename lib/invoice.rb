ce6ad7a8b3e1730f9de1b036ca8529f1346471c8
JamisonOrdway <jamisonordway@gmail.com>
2018-04-14 16:05:11 -0600

    Invoice WIP

--- lib/invoice.rb
+++ lib/invoice.rb
@@ -2,16 +2,42 @@ require 'bigdecimal'
 require 'time'
 
 class Invoice
+  attr_reader :attributes,
+              :parent
 
   def initialize(invoice, parent)
     @attributes = {
-      id:           invoice[:id],
+      id:           invoice[:id].to_i,
       customer_id:  invoice[:customer_id].to_i,
       merchant_id:  invoice[:merchant_id].to_i,
       status:       invoice[:status],
-      created_at:   Time.parse(item[:created_at]),
-      updated_at:   Time.parse(item[:updated_at])
+      created_at:   Time.parse(invoice[:created_at]),
+      updated_at:   Time.parse(invoice[:updated_at])
     }
     @parent = parent
+  end
+
+  def id
+    attributes[:id]
+  end
+
+  def customer_id
+    attributes[:customer_id]
+  end
+
+  def merchant_id
+    attributes[:merchant_id]
+  end
+
+  def status
+    attributes[:status]
+  end
+
+  def created_at
+    attributes[:created_at]
+  end
+
+  def updated_at
+    attributes[:updated_at]
   end
 end
--- test/invoice_test.rb
+++ test/invoice_test.rb
@@ -1,17 +1,39 @@
 require './test/test_helper'
-require_relative '../lib/item'
+require_relative '../lib/invoice'
 
 class InvoiceTest < Minitest::Test
+  attr_reader :invoice
 
   def setup
-       invoice = Invoice.new({
-        id:           invoice[:id],
-        customer_id:  invoice[:customer_id].to_i,
-        merchant_id:  invoice[:merchant_id].to_i,
-        status:       invoice[:status],
-        created_at:   Time.parse(item[:created_at]),
-        updated_at:   Time.parse(item[:updated_at])
-      } 'parent')
+       @invoice = Invoice.new({
+        id:           '9',
+        customer_id:  '6',
+        merchant_id:  '2'.to_i,
+        status:       'pending',
+        created_at: '2017-12-13 17:42:32 UTC',
+        updated_at: '2017-12-13 17:42:32 UTC',
+      },
+      'parent')
+  end
+
+  def test_it_exists
+    assert_instance_of Invoice, @invoice
+  end
+
+  def test_it_has_attributes
+    assert_equal 9, @invoice.attributes[:id]
+    assert_equal 6, @invoice.attributes[:customer_id]
+    assert_equal 2, @invoice.attributes[:merchant_id]
+    assert_equal 'pending',@invoice.attributes[:status]
+  end
+
+
+
+  def test_it_can_show_times_when_created_and_updated
+    creation_date = '2017-12-13 17:42:32 UTC'
+    updated_date = '2017-12-13 17:42:32 UTC'
+    assert_equal creation_date, @invoice.attributes[:created_at].to_s
+    assert_equal updated_date, @invoice.attributes[:updated_at].to_s
   end
 
 
