2c66f4f63bbc03c8b1fc2238976a8d25e8855a97
JamisonOrdway <jamisonordway@gmail.com>
2018-04-14 13:21:03 -0600

    Resolves merge conflict

--- lib/repository.rb
+++ lib/repository.rb
@@ -39,8 +39,6 @@ module Repository
     instances.delete(instance)
   end
 
-<<<<<<< HEAD
-=======
   def create_new_instance_with_dates(instances, attributes, klass)
     new_id = (instances.map(&:id).max + 1)
     attributes[:id] = new_id
@@ -50,5 +48,5 @@ module Repository
     end
     instances << klass.new(attributes, self)
   end
->>>>>>> 8332c6ebd163ab3d2cbf89ae68aa6341f96593a0
+
 end
