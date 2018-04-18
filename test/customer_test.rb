7877a58c0c4e7ab15d32ffedf1e33894e11cbefb
JamisonOrdway <jamisonordway@gmail.com>
2018-04-18 14:12:15 -0600

    Adds customer test

--- test/customer_test.rb
+++ test/customer_test.rb
@@ -30,4 +30,17 @@ class CustomerTest < Minitest::Test
     assert_equal creation_date, @customer.created_at.to_s
     assert_equal updated_date, @customer.updated_at.to_s
   end
-end
+
+#   def test_it_has_customer_information
+#     expected_created_date = Time.parse('2012-03-27 14:58:15 UTC')
+#     expected_updated_date = Time.parse('2012-03-27 14:58:15 UTC')
+#
+#     assert_equal 0, customer.id
+#     assert_equal 'Joey', customer.first_name
+#     assert_equal 'Ondricka', customer.last_name
+#     assert_equal 'joey', customer.first_name
+#     assert_equal 'ondricka', customer.last_name
+#     assert_equal expected_created_date, customer.created_at
+#     assert_equal expected_updated_date, customer.updated_at
+#   end
+# end
